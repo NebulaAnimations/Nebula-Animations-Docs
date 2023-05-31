91a2d5d0305736cb71842bab8cdb7501a9879c7c
Nebula Animations <j.falanga77@gmail.com>
2023-05-31 11:54:11 -0400

    Deploy website - based on

---
+++ assets/js/17ba1b70.f18480bb.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocuments_nebula_animations_docs=self.webpackChunkdocuments_nebula_animations_docs||[]).push([[458],{1694:a=>{a.exports=JSON.parse('{"title":"Magic Decal","description":"TBA","slug":"/category/magic-decal","permalink":"/category/magic-decal","navigation":{"next":{"title":"Documentation Coming Soon","permalink":"/magic_decal/Introduction"}}}')}}]);
